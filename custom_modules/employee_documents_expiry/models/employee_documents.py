693f6388f0d024c489822218da3df14f83cfebc8
Mritunjay Sharma <mritunjaysharma394@users.noreply.github.com>
2022-03-17 18:20:32 +0000

    Automated autoyapf fixes

--- custom_modules/employee_documents_expiry/models/employee_documents.py
+++ custom_modules/employee_documents_expiry/models/employee_documents.py
@@ -29,21 +29,6 @@ class HrEmployeeDocument(models.Model):
     _name = 'hr.employee.document'
     _description = 'HR Employee Documents'
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     def mail_reminder(self):
         now = datetime.now() + timedelta(days=1)
         date_now = now.date()
