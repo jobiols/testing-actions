b831344c95e4aaad96950d97a4d3133770c7de7c
autoblack <cclauss@users.noreply.github.com>
2022-03-25 23:44:57 +0000

    fixup: Format Python code with Black

--- custom_modules/auto_backup/models/db_backup.py
+++ custom_modules/auto_backup/models/db_backup.py
@@ -40,7 +40,9 @@ class DbBackup(models.Model):
         compute="_compute_name", store=True, help="Summary of this backup process"
     )
     folder = fields.Char(
-        default=lambda self: self._default_folder(),        help="Absolute path for storing the backups",        required=True,
+        default=lambda self: self._default_folder(),
+        help="Absolute path for storing the backups",
+        required=True,
     )
     days_to_keep = fields.Integer(
         required=True,
