98c9cf1346bda123232b281810fc51969dcf65ee
Jorge Obiols <jorge.obiols@gmail.com>
2022-03-25 21:28:00 -0300

    [FIX]

--- custom_modules/auto_backup/__manifest__.py
+++ custom_modules/auto_backup/__manifest__.py
@@ -19,9 +19,7 @@
     "depends": ["mail"],
     "data": [
         "data/ir_cron.xml",
-        "data/mail_message_subtype.xml",
-        "security/ir.model.access.csv",
-        "view/db_backup_view.xml",
+        "data/mail_message_subtype.xml",        "security/ir.model.access.csv",        "view/db_backup_view.xml",
     ],
     "installable": True,
     "external_dependencies": {"python": ["pysftp"]},
